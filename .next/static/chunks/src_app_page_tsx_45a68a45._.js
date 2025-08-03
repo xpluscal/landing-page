640c908487b7c2ad3aa5e3e2255616f89e2df8f2
Calvin Hoenes <calvinhoenes@gmail.com>
2025-08-03 16:46:08 +1000

    Deploy: 2025-08-03T06:46:08.721Z

--- .next/static/chunks/src_app_page_tsx_45a68a45._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_ed39b48c._.js",
-  "static/chunks/node_modules_3a757a30._.js"
-],
-    source: "dynamic"
-});
